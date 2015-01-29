ddfc84f8f1a21ce5b5b30734b001979bffaf9269
Jeremy Peter <jeremy.peter@meltmedia.com>
2015-01-28 17:19:18 -0700

    Making CSP work

--- server.config.js
+++ server.config.js
@@ -1,10 +1,16 @@
 module.exports = {
 
+  // TODO: set `session.secret` to environment variable
   session: {
     cookieName: 'session',
-    secret: 'sdflkj3oiuevnsli2340958xs230498sfd',
+    secret: 'Will Replace Later',
     duration: 30 * 60 * 1000,
     activeDuration: 5 * 60 * 1000
+  },
+
+  // TODO: add properties once we get up and running
+  csp: {
+    reportUri: ['/report-violations']
   }
 
 };
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var session = require('client-sessions');
 var csrf = require('csurf');
 var helmet = require('helmet');
 var conf = require('./server.config');
-var sess = require('express-session');
 var app = express();
 
 
@@ -12,18 +11,32 @@ var app = express();
 // Middleware
 ////////////////////////////////
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({ type: ['json', 'application/csp-report'] }));
+
+// Need to log Content Security Privacy reports before calling csrf because 
+// csrf will throw `Error: invalid csrf token` error
+app.post(conf.csp.reportUri, function (req, res) {
+  console.log(req.body);
+  res.sendStatus(200);
+ });
+
 app.use(session(conf.session));
 
 // IMPORTANT: csrf needs to be after any cookie and session initialization
 app.use(csrf());
+
+// Need to set csrf cookie for Angular 
 app.use(function(req, res, next) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   next();
 });
+
 app.use(helmet());
+app.use(helmet.contentSecurityPolicy(conf.csp));
 
 app.use(express.static(__dirname + '/dist'));
+
 
 
 
@@ -31,4 +44,13 @@ app.use(express.static(__dirname + '/dist'));
 // Routes 
 ////////////////////////////////
 
-app.listen(3000);
+
+
+
+/////////////////////////////////
+// Start Server
+////////////////////////////////
+
+app.listen(3000, function(){
+  console.log('Listenting on port 3000');
+});
